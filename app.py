5d5480348a259a2a611c55e3d638246bdd6db72a
Anna Warno <anna.warno@7bulls.com>
2020-05-31 21:42:49 +0200

    css fixed

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from chartScripts.governmentSatisfaction import govSatisfaction
 from chartScripts.world_map_plot import world_map_plot
 from chartScripts.time_series import predict_series
 
-app = dash.Dash(suppress_callback_exceptions=True, external_stylesheets=[
+app = dash.Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[
     "https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap-grid.min.css"])
 
 server = app.server
